d27e225f80f89805e87bd221663482404fca43ab
Cedric <cedric.willekens@hotmail.com>
2017-10-12 09:25:34 +0200

    Define the files used by goal

---
+++ src/nl/tudelft/core/file/ACT2GFile.java
@@ -0,0 +1,32 @@
+package nl.tudelft.core.file;
+
+import com.intellij.extapi.psi.PsiFileBase;
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.psi.FileViewProvider;
+import nl.tudelft.core.language.GoalLanguage;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+public class ACT2GFile extends PsiFileBase {
+    protected ACT2GFile(@NotNull FileViewProvider viewProvider) {
+        super(viewProvider, GoalLanguage.instance);
+    }
+
+    @NotNull
+    @Override
+    public FileType getFileType() {
+        return ACT2GFileType.INSTANCE;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Action specification file";
+    }
+
+    @Override
+    public Icon getIcon(int flags) {
+        return super.getIcon(flags);
+    }
+}
---
+++ src/nl/tudelft/core/file/MAS2GFile.java
@@ -0,0 +1,31 @@
+package nl.tudelft.core.file;
+
+import com.intellij.extapi.psi.PsiFileBase;
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.psi.FileViewProvider;
+import nl.tudelft.core.language.GoalLanguage;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+public class MAS2GFile extends PsiFileBase {
+    protected MAS2GFile(@NotNull FileViewProvider viewProvider) {
+        super(viewProvider, GoalLanguage.instance);
+    }
+
+    @NotNull
+    @Override
+    public FileType getFileType() {
+        return MAS2GFileType.INSTANCE;
+    }
+
+    @Override
+    public String toString() {
+        return "Multi-agent specification file";
+    }
+
+    @Override
+    public Icon getIcon(int flags) {
+        return super.getIcon(flags);
+    }
+}
