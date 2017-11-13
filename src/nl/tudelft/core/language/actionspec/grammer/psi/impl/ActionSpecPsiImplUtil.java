2c740bd193d1f45999849d7305bcafc4e77fbbdf
Cedric <cedric.willekens@hotmail.com>
2017-11-13 10:37:20 +0100

    Update getReference function

--- src/nl/tudelft/core/language/actionspec/grammer/psi/impl/ActionSpecPsiImplUtil.java
+++ src/nl/tudelft/core/language/actionspec/grammer/psi/impl/ActionSpecPsiImplUtil.java
@@ -37,7 +37,7 @@ public class ActionSpecPsiImplUtil {
     }
 
     public static PsiReference getReference(ActionSpecBasicTerm term) {
-        return GoalPsiUtilImpl.getReference()
+        return GoalPsiUtilImpl.getReference();
     }
 
 }
