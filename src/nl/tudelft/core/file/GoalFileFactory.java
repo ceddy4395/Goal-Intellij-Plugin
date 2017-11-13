e3f17c16ab91115585589636a269ca913e4f8c72
Cedric <cedric.willekens@hotmail.com>
2017-11-13 16:16:54 +0100

    Create File types for modules

--- src/nl/tudelft/core/file/GoalFileFactory.java
+++ src/nl/tudelft/core/file/GoalFileFactory.java
@@ -3,6 +3,7 @@ package nl.tudelft.core.file;
 import com.intellij.openapi.fileTypes.*;
 import nl.tudelft.core.language.actionspec.file.ACT2GFileType;
 import nl.tudelft.core.language.mas.file.MAS2GFileType;
+import nl.tudelft.core.language.module.file.MOD2GFileType;
 import nl.tudelft.core.language.prolog.file.PrologFileType;
 import org.jetbrains.annotations.NotNull;
 
@@ -12,5 +13,6 @@ public class GoalFileFactory extends FileTypeFactory {
         consumer.consume(MAS2GFileType.INSTANCE, "mas2g");
         consumer.consume(ACT2GFileType.INSTANCE, "act2g");
         consumer.consume(PrologFileType.INSTANCE, "pl");
+        consumer.consume(MOD2GFileType.INSTANCE, "mod2g");
     }
 }
